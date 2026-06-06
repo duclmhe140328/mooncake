eae3189320ff7300a445639c092edbbaf93c41aa
Lê Minh Đức <86581679+duclmhe140328@users.noreply.github.com>
2026-06-06 14:55:23 +0700

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,35 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
-const orderController = require('../controllers/orderController');
+const orderController =
+    require('../controllers/orderController');
 
-/*
-|--------------------------------------------------------------------------
-| VNPAY ROUTES
-|--------------------------------------------------------------------------
-| Đặt các route cố định lên đầu để tránh xung đột với route động sau này.
-*/
+router.get(
+    '/vnpay_return',
+    orderController.vnpayReturn
+);
 
-router.get('/vnpay_return', (req, res, next) => {
-    console.log('Đã nhận VNPay Return:', req.query);
+router.get(
+    '/vnpay_ipn',
+    orderController.vnpayIpn
+);
 
-    return orderController.vnpayReturn(req, res, next);
-});
+router.post(
+    '/',
+    orderController.createOrder
+);
 
-router.get('/vnpay_ipn', (req, res, next) => {
-    console.log('Đã nhận VNPay IPN:', req.query);
-
-    return orderController.vnpayIpn(req, res, next);
-});
-
-/*
-|--------------------------------------------------------------------------
-| ORDER ROUTES
-|--------------------------------------------------------------------------
-*/
-
-router.post('/', orderController.createOrder);
-
-router.get('/', orderController.getOrders);
+router.get(
+    '/',
+    orderController.getOrders
+);
 
 module.exports = router;
